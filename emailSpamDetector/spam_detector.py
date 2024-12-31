0a21e05d6b1f76ac9ecd90112ba8ebad569b5dbb
Varghese, Derric <derricvarghese@my.unt.edu>
2024-12-31 08:43:33 -0600

    Create the Naive Bayes Classifier

--- emailSpamDetector/spam_detector.py
+++ emailSpamDetector/spam_detector.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
 
 def load_data():
     url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
@@ -12,6 +14,14 @@ def train_model(data):
     y = data['label']
     vectorizer = TfidfVectorizer()
     x = vectorizer.fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    model = MultinomialNB()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+    
+    return accuracy, report
 
 
 if __name__ == "__main__":
