77e5f90dd66ddc055338843807fa2c97f1fb08cf
Varghese, Derric <derricvarghese@my.unt.edu>
2024-12-31 11:13:56 -0600

    Create Predict File

--- emailSpamDetector/spam_detector.py
+++ emailSpamDetector/spam_detector.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
 
 def load_data():
     url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
@@ -20,10 +22,15 @@ def train_model(data):
     y_pred = model.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
     report = classification_report(y_test, y_pred)
+    joblib.dump(model, 'spam_detector_model.pkl')
+    joblib.dump(vectorizer, 'spam_detector_vectorizer.pkl')
+
     
     return accuracy, report
 
 
 if __name__ == "__main__":
     data = load_data()
-    print(data)
+    accuracy, report = train_model(data)
+    print(f"Accuracy: {accuracy}")
+    print(f"Classification Report:\n{report}")
